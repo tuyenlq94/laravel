dbbfaf647ed3fd5fd049b95d1673a28fde3e9164
tuyenlq94 <tuyenicolor@gmail.com>
2022-04-12 16:41:57 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+	return view( 'welcome' );
+});
+
+Route::get( 'welcome', function () {
+	return 'Hello word';
+});
+
+Route::get( 'user/{id}', function( $id ) {
+	return "ID user: $id";
+});
+
+Route::get( 'user/{id?}', function( $id = null ) {
+	if ( ! $id ) {
+		return 'Mời nhập ID:';
+	}
+	return $id;
+} );
+Route::get( 'user/{id}/comment/{commentId}', function ( $id, $commentId ) {
+	return "User ID: $id and comment ID: $commentId";
+} )->where( 'id', '[0-9]+' )->where( 'commentId', '[a-zA-Z0-9]+' );
